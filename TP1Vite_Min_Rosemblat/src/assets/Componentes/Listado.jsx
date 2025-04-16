b74c911aea972c42080b0632f31e9540f9bf48e5
TOMAS LEANDRO MIN <48386410@ort>
2025-04-16 09:06:02 -0300

    Inicio ap.jsx

---
+++ TP1Vite_Min_Rosemblat/src/assets/Componentes/Listado.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Listado.css';
+
+function Listado() 
+{
+    return (
+    <>
+        <h1>ADMINISTRADOR DE PACIENTES</h1>
+            <div class="container"/>
+            <div class="row"/>
+            <div class="one-half column"/>
+            <h2>Crear mi Cita</h2>
+            <div>
+        </div>
+    </>
+    );
+}
