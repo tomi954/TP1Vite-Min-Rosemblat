6dfe18d684b2ded1cf0413ad3e79d7cc353344f6
TOMAS LEANDRO MIN <48386410@ort>
2025-04-16 08:35:12 -0300

    2/4 componentes

---
+++ TP1Vite_Min_Rosemblat/src/Cita.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import './Cita.css';
+
+
+function Cita()
+{
+    return(
+    <div class="one-half column">
+      <h2>Administra tus citas</h2>
+      <div class="cita">
+        <p>Mascota: <span>Nina</span></p>
+        <p>Dueño: <span>Martin</span></p>
+        <p>Fecha: <span>2021-08-05</span></p>
+        <p>Hora: <span>08:20</span></p>
+        <p>Sintomas: <span>Le duele la pierna</span></p><button class="button elimnar u-full-width">Eliminar ×</button>
+      </div>
+      <div class="cita">
+        <p>Mascota: <span>Sifon</span></p>
+        <p>Dueño: <span>Flecha</span></p>
+        <p>Fecha: <span>2023-08-05</span></p>
+        <p>Hora: <span>09:24</span></p>
+        <p>Sintomas: <span>Duerme mucho</span></p><button class="button elimnar u-full-width">Eliminar ×</button>
+      </div>
+      <div class="cita">
+        <p>Mascota: <span>Floki</span></p>
+        <p>Dueño: <span>Ari</span></p>
+        <p>Fecha: <span>2023-08-05</span></p>
+        <p>Hora: <span>16:15</span></p>
+        <p>Sintomas: <span>No está comiendo</span></p><button class="button elimnar u-full-width">Eliminar ×</button>
+      </div>
+    </div>
+    );
+}
+export default Cita;
